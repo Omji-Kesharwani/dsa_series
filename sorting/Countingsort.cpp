a7a585cbe2e32d129eea0f72cb1db54425e82836
Omji-Kesharwani <omjikesharwani000@gamil.com>
2024-08-10 16:39:07 +0530

    Counting Sort

---
+++ sorting/Countingsort.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> countSort(vector<int>& inputArray)
+{
+  int N=inputArray.size();
+  int M=0;
+  M=*max_element(inputArray.begin(),inputArray.end());
+  vector<int>countArray(M+1,0);
+
+
+  for(int i=0;i<N;i++)
+  countArray[inputArray[i]]++;
+
+  for(int i=1;i<=M;i++)
+  {
+    countArray[i]+=countArray[i-1];
+  }
+  vector<int>outputArray(N);
+  for(int i=N-1;i>=0;i--)
+  {
+    outputArray[countArray[inputArray[i]]-1]=inputArray[i];
+    countArray[inputArray[i]]--;
+  }
+  return outputArray;
+}
+int main()
+
+{
+
+    // Input array
+    vector<int> inputArray = { 4, 3, 12, 1, 5, 5, 3, 9 };
+
+    // Output array
+    vector<int> outputArray = countSort(inputArray);
+
+    for (int i = 0; i < inputArray.size(); i++)
+        cout << outputArray[i] << " ";
+
+    return 0;
+}
